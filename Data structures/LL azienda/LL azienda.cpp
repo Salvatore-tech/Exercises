8d87beca975fcd583a0a7ebcec248f9a553ebff8
Salvatore-tech <salvatore.tech99@gmail.com>
2020-01-16 17:51:36 +0100

    Linked List

---
+++ Data structures/LL azienda/LL azienda.cpp
@@ -0,0 +1,25 @@
+/* LL azienda.cpp : This file contains the 'main' function. Program execution begins and ends there.
+Progettare ed implementare una lista (linked list) di impiegati di un'azienda.
+Un impiegato ha un nome ed un salario. Un manager è un impiegato cui viene corrisposto un bonus oltre al salario.
+1) Creare una lista di impiegati e stamparla a video sfruttando il principio del polimorfismo
+2) Implementare l'overload dell'operatore + in modo da sommare i salari di tutti gli impiegati.
+*/
+
+class Node {
+
+};
+
+class LinkedList :public Node {
+
+};
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+}
