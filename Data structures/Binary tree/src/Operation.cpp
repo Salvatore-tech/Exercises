1c3284f332aa7f261d2d71eca0958acd0ec15c7a
Salvatore-tech <salvatore.tech99@gmail.com>
2020-01-28 17:10:17 +0100

    Header added

--- Data structures/Binary tree/src/Operation.cpp
+++ Data structures/Binary tree/src/Operation.cpp
@@ -7,47 +7,79 @@ void Tree::insert(int key)
 {
     Node* nuovoNodo = new Node;
     nuovoNodo->key = key;
-    nuovoNodo->left = nuovoNodo->right = nullptr;
-    if (root == nullptr)
+    nuovoNodo->left = nuovoNodo->right = nuovoNodo->parent = nullptr;
+    Node* x = root;
+    Node* y = nullptr;
+    while (x != nullptr)
+    {
+        y = x;
+        if (key < x->key)
+            x = x->left;
+        else
+            x = x->right;
+    }
+    nuovoNodo->parent = y;
+    if (y == nullptr)
         root = nuovoNodo;
+    else if (nuovoNodo->key < nuovoNodo->parent->key)
+        nuovoNodo->parent->left = nuovoNodo;
     else
-    {
-        Node* current = root;
-        Node* parent = nullptr;
-        while (current != nullptr)
-        {
-            parent = current;
-            if (key < current->key)
-                current = current->left;
-            else
-                current = current->right;
-        }
-        if (key < parent->key)
-            parent->left = nuovoNodo;
-        else // (key > parent->key&& current == nullptr)
-            parent->right = nuovoNodo;
-    }
+        nuovoNodo->parent->right = nuovoNodo;
 }
 
 
-int Tree::minSearch(Node* p)
+Node* Tree::maxSearch(Node* p)
 {
-    Node* parent = nullptr;
-    while (p != nullptr)
-    {
-        parent = p;
+    while (p->right != nullptr)
+        p = p->right;
+    return p;
+}
+
+Node* Tree::minSearch(Node* p)
+{
+    while (p->left != nullptr)
         p = p->left;
-    }
-    return parent->key;
+    return p;
 }
-/*
-int Tree::minSearch(Node* p)
-{
-    if (p != nullptr)
-        minSearch(p->left);
-    else 
-        return p->key;
 
-    
+//Il successore di un generico nodo v è il più piccolo nodo maggiore dello stesso
+Node* Tree::successorSearch(Node* p)
+{
+    if (p->right != nullptr)
+        return(minSearch(p->right));
+    Node* y = p->parent;
+    while (y != nullptr && p == p->parent->right)
+    {
+        Node* x = y;
+        y = p->parent;
+    }
+    return y;
 }
-*/
+
+Node* Tree::predecessorSearch(Node* p)
+{
+    if (p->left != nullptr)
+        return(maxSearch(p->left));
+    Node* y = p->parent;
+    while (y!=nullptr && p==p->parent->left)
+    {
+        Node* x = y;
+        y = p->parent;
+    }
+    return y;
+}
+
+void Tree::trasplant(Node* u, Node* v)
+{
+    if (u->parent == nullptr)
+        root = v;
+    else if (u == u->parent->left)
+        u->left->parent = v;
+    else
+        u->parent->right = v;
+    if (v != nullptr)
+        v->parent = u->parent;
+
+
+
+}
